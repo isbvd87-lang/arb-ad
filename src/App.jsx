1a6bd55e844794e457fd42f2242f95e5f90e5018
isbvd87-lang <isbvd87@gmail.com>
2026-06-29 03:24:02 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main_Page from "./Main_Page";
 import Login from "./Login";
 
 // export const serverRoute = "http://localhost:8080";
-export const serverRoute = "https://bracelet-se.onrender.com";
+export const serverRoute = "https://bracelet-se-08z6.onrender.com";
 export const token = localStorage.getItem("token");
 function App() {
   return (
